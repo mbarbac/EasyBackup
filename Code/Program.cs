d4c9e238ed8d56abf70d2f1aec9530c2ab0d0d4e
Moises Barba Cebeira <mbarbac@hotmail.com>
2025-03-16 17:51:35 +0100

    xxx

---
+++ Code/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace EasyBackup;
+
+// ========================================================
+/// <summary>
+/// Program entry point.
+/// </summary>
+internal class Program
+{
+    /// <summary>
+    /// Program entry point.
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
